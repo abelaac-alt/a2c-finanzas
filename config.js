900eb115371edf68c58636a7d8d01877391c5a51
abelaac-alt <abel.atero@gmail.com>
2026-07-12 16:10:02 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.A2C_CONFIG = {
-  SUPABASE_URL: 'https://TU-PROYECTO.supabase.co',
-  SUPABASE_ANON_KEY: 'TU_CLAVE_PUBLICA'
+  SUPABASE_URL: 'https://nadlmxddafwyihobktoe.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_6wkiMwEaTOO4p6FWKBjm2Q_aBGpGSAJ'
 };
