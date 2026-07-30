f775f3bef1d6403b41239f75a3698813381cdd73
abelaac-alt <abel.atero@gmail.com>
2026-07-30 17:17:14 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,1 @@
-// Script de inicialización de UI (Para conectar con app.bundle.js)
-console.log('UI Moderna inicializada.');
-// Aquí se enlazarán los eventos visuales del nuevo diseño con la lógica existente.
+/* A2C 5.0: usa app.bundle.js generado desde /src. */
